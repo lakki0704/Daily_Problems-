18200d377493d0744600f5f99d400f1c4b14e640
Lakshmi K Sathyan <78313062+lakki0704@users.noreply.github.com>
2023-01-30 19:16:43 +0530

    Update day1.java

--- day1.java
+++ day1.java
@@ -25,4 +25,20 @@ class Solution {
     }
 }
 
+/*
+question:
+Given an array arr[] of positive integers of size N. Reverse every sub-array group of size K.
+
+Note: If at any instance, there are no more subarrays of size greater than or equal to K, then reverse the last subarray (irrespective of its size). You shouldn't return any array, modify the given array in-place.
+
+Example 1:
+
+Input:
+N = 5, K = 3
+arr[] = {1,2,3,4,5}
+Output: 3 2 1 5 4
+Explanation: First group consists of elements
+1, 2, 3. Second group consists of 4,5.
+*/
+
 
