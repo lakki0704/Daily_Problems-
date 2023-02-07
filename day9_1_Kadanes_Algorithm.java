315976e4c9eef6c479ebf7eedce8b69dff89efcd
Lakshmi K Sathyan <78313062+lakki0704@users.noreply.github.com>
2023-02-07 07:11:02 +0530

    Create day9_1_Kadanes_Algorithm.java

---
+++ day9_1_Kadanes_Algorithm.java
@@ -0,0 +1,30 @@
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+    
+        int curr=0;
+        int max =Integer.MIN_VALUE;
+        for(int i=0; i<n; i++)
+        {
+            curr = curr+arr[i];
+            if(curr>max)
+            {
+                max=curr;
+            }
+            if(curr<0)
+            {
+                curr=0;
+            }
+        }
+        
+        return max;
+        
+    }
+    
+}
+
