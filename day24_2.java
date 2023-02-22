3d3c3882265032e7de04caa9dd69a0be3f9b0893
Lakshmi K Sathyan <78313062+lakki0704@users.noreply.github.com>
2023-02-22 10:31:03 +0530

    Queue

---
+++ day24_2.java
@@ -0,0 +1,25 @@
+//rotate the array counter clockwise d positions
+
+class Solution
+{
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    static void rotateArr(int arr[], int d, int n)
+    {
+        // add your code here
+        Queue<Integer> q = new LinkedList<Integer>();;
+        for(int i=0; i<n; i++)
+        {
+            q.add(arr[i]);
+        }
+        
+        for(int i=0; i<d; i++)
+        {
+            q.add(q.poll());
+        }
+        
+        for(int i=0; i<n ; i++)
+        {
+            arr[i] = q.poll();
+        }
+    }
+}
